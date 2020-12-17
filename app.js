7ea173fea0f5a6c2f683e4ca21c2f08e3d05d9b0
baphunguyen <baphunguyen99@gmail.com>
2020-12-17 12:49:51 +0700

    ada

--- app.js
+++ app.js
@@ -14,6 +14,14 @@ db.on('error', (err) => {
     console.log('DB connection error:', err.message);
 })
 
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "https://quanlynhatro-online.herokuapp.com"); // update to match the domain you will make the request from
+    res.header(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept"
+    );
+    next();
+});
 
 app.use(morgan("dev"));
 app.use(bodyParser.json());
