8d0f5fd7b39c67791b45f31d192c01ce9c7f3d27
baphunguyen <baphunguyen99@gmail.com>
2020-12-18 23:11:28 +0700

    adad

--- app.js
+++ app.js
@@ -16,12 +16,12 @@ db.on('error', (err) => {
 })
 
 app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "https://quanlynhatro-online.herokuapp.com"); // update to match the domain you will make the request from
-    res.header(
-      "Access-Control-Allow-Headers",
-      "Origin, X-Requested-With, Content-Type, Accept"
-    );
-    next();
+  res.header("Access-Control-Allow-Origin", "https://quanlynhatro-online.herokuapp.com"); // update to match the domain you will make the request from
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authentication"
+  );
+  next();
 });
 
 app.use(bodyParser());
