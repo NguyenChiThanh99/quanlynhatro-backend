3771401c76bac74ec55b9d03cd5afb9b35f192fe
baphunguyen <baphunguyen99@gmail.com>
2020-12-21 21:28:23 +0700

    Adad

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -5,16 +5,16 @@ exports.authenticateToken = async function(req, res, next) {
     try {
         const authHeader = req.headers['authorization'];
         const token = authHeader && authHeader.split(' ')[1]
-        if (token == null) return res.status(401).json({status: false, message: 'Unauthorized user!'}) //if there isn't any token
+        if (token == null) return res.json({status: false, message: 'Unauthorized user!'}) //if there isn't any token
 
         const jwtuser = await jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
-        if (!jwtuser) res.status(401).json({status: false, message: 'Unauthorized user!'})
+        if (!jwtuser) res.json({status: false, message: 'Unauthorized user!'})
 
         const user = await User.findOne({ email: jwtuser.user.email, isDeleted: false });
-        if (!user) return res.status(401).json({status: false, message: 'Unauthorized user!'})
+        if (!user) return res.json({status: false, message: 'Unauthorized user!'})
         req.user = user;
         next()
     } catch(err) {
-        return res.status(401).json({status: false, message: 'Unauthorized user!'})
+        return res.json({status: false, message: 'Unauthorized user!'})
     }
 }
